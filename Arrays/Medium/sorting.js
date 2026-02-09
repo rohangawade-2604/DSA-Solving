39b7ce0d11925ba4e55f99e84e9267e68089f73f
rohangawade-2604 <rohangawade2604@gmail.com>
2026-02-09 18:53:18 +0530

    code

--- Arrays/Medium/sorting.js
+++ Arrays/Medium/sorting.js
@@ -1,1 +1,19 @@
+// Medium 4️⃣ – Sort an array in ascending order 
+
+let arr = [20 , 10 , 30, 40]
+
+for(i=0; i<arr.length-1; i++){
+    console.log(arr[i])
+    for(j=i+1; j<arr.length; j++){
+        
+        if(arr[i] >= arr[j]){
+           let temp = arr[i];
+           arr[i] = arr[j];
+           arr[j] = temp
+        }
+    }
+}
+
+console.log("array is sorted",arr)
+
 
