e751507d9388ed2d34ecfb0d9636cc26f7ec652a
rohangawade-2604 <rohangawade2604@gmail.com>
2026-02-09 13:35:21 +0530

    code

--- Arrays/Medium/secondlargest.js
+++ Arrays/Medium/secondlargest.js
@@ -13,7 +13,7 @@ for(let i = 0; i<arr.length; i++){
          second = biggest
         biggest = arr[i];
     }
-   else if(arr[i] > second && arr[i] < biggest){
+   else if(arr[i] > second && arr[i] < biggest){     //else if part is chatgpt , just for specifications nothing else , it can run the code without the else if
         second = arr[i]
    }
 }
