acc14ca4494901df725e60533204e7fd48840999
rohangawade-2604 <rohangawade2604@gmail.com>
2026-02-13 01:36:08 +0530

    code

---
+++ Arrays/Medium/missing.js
@@ -0,0 +1,16 @@
+// 🟡 Medium 2️⃣ – Find missing number
+// (LeetCode 268 – simplified)
+
+let arr = [3, 0, 1]             // 👉 Output: 2
+
+
+let n = arr.length
+
+
+for(let i = 0; i<n; i++){
+    console.log(arr[i])
+
+
+
+}
+
