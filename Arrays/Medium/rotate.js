a3ad4305f08986385fa933a5620427d6da522d41
rohangawade-2604 <rohangawade2604@gmail.com>
2026-02-16 15:07:37 +0530

    cde

---
+++ Arrays/Medium/rotate.js
@@ -0,0 +1,13 @@
+// 🟡 Medium 3️⃣ – Rotate array by K steps (right rotation)
+let arr = [1, 2, 3, 4, 5]
+let k = 2           // 👉 Output:      [4, 5, 1, 2, 3]
+
+
+
+
+
+
+
+
+console.log(arr1)
+// console.log(arr2)
